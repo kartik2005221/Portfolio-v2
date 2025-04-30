c568722bb27db6d60cb26711fa1ddc53c131712d
Kartik <kartik2005221@gmail.com>
2025-04-30 11:28:48 +0530

    30-04-2025, Enhance styling and layout for improved user experience; add smooth scrolling behavior

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,3 +13,16 @@ function omniSecRepo() {
 function foxExt() {
     window.open('https://addons.mozilla.org/en-US/firefox/addon/google-labs-fix-air/')
 }
+
+//smooth scrolling
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute('href'));
+//         if (target) {
+//             target.scrollIntoView({
+//                 behavior: 'smooth', block: 'start'
+//             });
+//         }
+//     });
+// });
